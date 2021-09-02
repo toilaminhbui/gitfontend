492ed24d06c114937096aa1a697695fea84b5284
toilaminhbui <toilaminhbui@gmail.com>
2021-09-02 18:10:45 +0700

    app changinga

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import logo from "./logo.svg"
 import "./App.css"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>The project will be updated soon</h1>
-        <a href="https://www.facebook.com/Van.Minh.Nekk">Bui Van Minh</a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={() => {
+            return (
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1>The project will be updated soon</h1>
+                  <a href="https://www.facebook.com/Van.Minh.Nekk">
+                    Bùi Văn Minh
+                  </a>
+                </header>
+              </div>
+            )
+          }}
+        />
+        <Route
+          component={() => {
+            return <h1>Có cái nịt trang này ko tồn tại</h1>
+          }}
+        />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
